4b825567e31f0e42dd688f7a9096b9f77d52d108
igorcastro-dsn <igorcastro.dsn@gmail.com>
2021-01-10 19:18:24 -0300

    ADD Circuit Breaker e Fallback com Hystrix

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.controller.dto.CompraDto;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDto;
@@ -19,6 +21,7 @@ public class CompraService {
 	@Autowired
 	private FornecedorClient fornecedorClient;
 
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
 	public Compra realizaCompra(CompraDto compra) {
 		InfoFornecedorDto fornecedor = fornecedorClient.obterInfoPorEstado(compra.getEndereco().getEstado());
 		LOGGER.info("Obtendo informações do fornecedor - Endereço: {}", fornecedor.getEndereco());
@@ -26,7 +29,16 @@ public class CompraService {
 		LOGGER.info("Realizando um pedido com {} itens", compra.getItens().size());
 		InfoPedidoDto pedido = fornecedorClient.realizarPedido(compra.getItens());
 		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		return new Compra(pedido.getId(), pedido.getTempoDePreparo(), compra.getEndereco().toString());
 	}
 
+	public Compra realizaCompraFallback(CompraDto compra) {
+		return new Compra(null, null, "Gerado pelo fallback");
+	}
 }
